68af8e18108d8c54d714893a80bd30124bc2972c
jonloyens <jonloyens@gmail.com>
2011-08-21 16:24:07 -0500

    fixed scoping errors in feed processing view

--- socialpakt_site/orders/views.py
+++ socialpakt_site/orders/views.py
@@ -10,15 +10,15 @@ from models import Order
 def foxyfeed(request):
     if request.POST and 'FoxyData' in request.POST:
         try:
-      		data = FoxyData.from_crypted_str(urllib.unquote_plus(request.POST['FoxyData']), settings.FOXYCART_DATAFEED_KEY)	# IMPORTANT: unquote_plus is necessary for the non-ASCII binary that FoxyCart sends.
-      		for transaction in data.transactions:
-        		order = Order(order_xml=str(data))
-        		order.save()
- 
-      		return HttpResponse('foxy')
- 
-    	except Exception, e:
-	  		# Something went wrong, handle the error...
-      		raise
- 
-  	return HttpResponseForbidden('Unauthorized request.')  # No FoxyData?  Not a POST?  We don't speak that.
+            data = FoxyData.from_crypted_str(urllib.unquote_plus(request.POST['FoxyData']), settings.FOXYCART_DATAFEED_KEY)	# IMPORTANT: unquote_plus is necessary for the non-ASCII binary that FoxyCart sends.
+            for transaction in data.transactions:
+                order = Order(order_xml=str(data))
+                order.save()
+
+            return HttpResponse('foxy')
+
+        except Exception, e:
+            # Something went wrong, handle the error...
+            raise
+
+    return HttpResponseForbidden('Unauthorized request.')  # No FoxyData?  Not a POST?  We don't speak that.
