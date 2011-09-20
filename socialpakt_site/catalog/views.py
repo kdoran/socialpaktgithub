d9c8e19f223a7e87595abfa9cfc530ea3d5692e6
jonloyens <jonloyens@gmail.com>
2011-09-19 22:17:20 -0500

    pulled in changes from preview site

--- socialpakt_site/catalog/views.py
+++ socialpakt_site/catalog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import ModelFormMixin, ProcessFormView
 from models import *
 from django.conf import settings
 from datetime import datetime
@@ -6,8 +7,13 @@ from datetime import datetime
 from django.shortcuts import get_object_or_404
 from django.contrib.contenttypes.models import ContentType
 
-class CatalogHomeView(TemplateView):
+from splash.models import *
+
+class CatalogHomeView(ModelFormMixin, ProcessFormView, TemplateView):
     template_name = "catalog/product.html"
+    form_class = SignupForm
+    success_url = "."
+    object = None
 
     def get_context_data(self, **kwargs):
         products = Product.objects.filter(active=True).order_by('-date_expires')
@@ -18,8 +24,19 @@ class CatalogHomeView(TemplateView):
             product = products[:1]
             product = product[0] if len(products) else None # need to refactor this
 
-        return {
+        context = {
             'product' : product,
             'can_add_to_cart' : product.date_expires >= datetime.now(),
             'product_variation_contenttype' : ContentType.objects.get(model="productvariation"),
         }
+
+        kwargs.update(context)
+
+        # call get context data for ModelFormMixin
+        model_form_context = ModelFormMixin.get_context_data(self, **kwargs)
+
+        kwargs.update(model_form_context)
+
+        print kwargs
+
+        return kwargs
