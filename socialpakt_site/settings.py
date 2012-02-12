a3e22d3f36cdc2cf5108cab58bb3a47d08e3ff6f
kevin <kev.doran@gmail.com>
2012-02-12 16:40:18 -0600

    asdf

--- socialpakt_site/settings.py
+++ socialpakt_site/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'socialpakt_prod0204',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'password',                  # Not used with sqlite3.
-        'HOST': '/tmp/',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
